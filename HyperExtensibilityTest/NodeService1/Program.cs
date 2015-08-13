cdeef3e767f2850b0f0969b7551e2606d1da89d8
Josh Heitz <jheitz1117@gmail.com>
2015-08-13 14:35:52 -0500

    Add Xml Documentation Files.
    -Add minimal configuration for HyperNode.

--- HyperExtensibilityTest/NodeService1/Program.cs
+++ HyperExtensibilityTest/NodeService1/Program.cs
@@ -1,4 +1,6 @@
-﻿using Hyper.WcfHosting;
+﻿using System;
+using System.ServiceModel;
+using Hyper.NodeServices;
 
 namespace NodeService1
 {
@@ -6,7 +8,20 @@ namespace NodeService1
     {
         static void Main(string[] args)
         {
-            
+            var host = new ServiceHost(HyperNodeService.Instance);
+
+            Console.WriteLine("Starting service...");
+            host.Open();
+
+            Console.WriteLine("Service started and is listening on the following addresses:");
+            foreach (var endpoint in host.Description.Endpoints)
+            {
+                Console.WriteLine("    " + endpoint.Address);
+            }
+
+            Console.ReadKey();
+
+            host.Close();
         }
     }
 }
